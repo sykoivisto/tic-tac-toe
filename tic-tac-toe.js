a2215ec169bfc7568f2186d8598e1d88d0e45114
sy <sykoivisto@gmail.com>
2022-10-01 15:49:37 -0700

    add logic to detect wins

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,8 +5,34 @@ const gameboard = (() => {
         2,0,2
     ]; // 0 = empty, 1 = player 1 (x's), 2 = player 2 (o's)
 
-    const setGrid = (gridSpace, val) => { //accepts the grid space and the value to set the grid space to
+    const checkGameState = (val) => {
+        //check for game wins and ties. if nothing found game continues. if game ends, alert is created, points are added, and board resets
+
+        if ( // check for wins
+            (grid[0] === grid[1] && grid[0] === grid[2] && grid[0] !== 0) ||//row wins
+            (grid[3] === grid[4] && grid[3] === grid[5] && grid[3] !== 0) ||
+            (grid[6] === grid[7] && grid[6] === grid[8] && grid[6] !== 0) ||
+            (grid[0] === grid[3] && grid[0] === grid[6] && grid[0] !== 0) ||//column wins
+            (grid[1] === grid[4] && grid[1] === grid[7] && grid[1] !== 0) ||
+            (grid[2] === grid[5] && grid[2] === grid[8] && grid[2] !== 0) ||
+            (grid[0] === grid[4] && grid[0] === grid[8] && grid[0] !== 0) ||//diag wins
+            (grid[2] === grid[4] && grid[2] === grid[6] && grid[2] !== 0)) {
+            
+            //do something (someone won)
+            //use the last placed mark to determine who won.
+            return val;
+
+        } else if (!grid.includes(0)) {//check if there are still moves left
+            //no moves left? tie!
+            return 3;
+        };
+    
+    };
+
+    const setGrid = (gridSpace, val) => { //accepts the grid space and the value (player) to set the grid space to. also returns the game state
         grid[gridSpace] = val;
+
+        return checkGameState(val);
     };
 
     const getGrid = () => {
@@ -38,7 +64,7 @@ const player = (name) => {
         getName,
         getScore
     };
-}
+};
 
 const gamecontroller = (() => {
     let turn = 1; //1 for player x turn, 2 for player o turn
@@ -57,7 +83,7 @@ const gamecontroller = (() => {
                 default:
                     break;
             };
-        }
+        };
     };
 
     const clearGrid = () => { //resets the DOM and gameboard.grid
@@ -69,30 +95,51 @@ const gamecontroller = (() => {
         };
 
         gameboard.reset();
-
+        renderGrid();
     };
 
-    const advanceTurn = () => {
+    const advanceTurn = () => {//if it's player 1's turn, make it player 2, otherwise, make it player 1 again
         if (turn === 1) {
             turn = 2;
         } else {
             turn = 1;
         }
-    }
+    };
 
     const click = (e) => {
         let target = (e.target.attributes.id.value);
 
         if (gameboard.getGrid()[target] === 0) { //check if the grid space is empty
-            gameboard.setGrid(target, turn);
+            let gamestate = (gameboard.setGrid(target, turn)); //add our player's mark to the board. setGrid also returns our game state
             advanceTurn();
             gamecontroller.renderGrid();
-        }
-    }
 
-    [...document.querySelectorAll('.grid-space')].forEach(function(item) {
+            if (gamestate !== 0) {
+                handleGameEnd(gamestate);
+            };
+        };
+    };
+
+    [...document.querySelectorAll('.grid-space')].forEach(function(item) {// our click listener on every grid space.
         item.addEventListener('click', click);
     });
+
+    const handleGameEnd = (state) => {
+        switch (state) {
+            case 1:
+                //handle player 1 win;
+                alert('player 1 wins')
+                break;
+            case 2:
+                //handle player 2 win
+                alert('player 2 wins')
+                break;
+            case 3:
+                //handle a tie
+                alert('its a tie')
+                break;
+        }
+    };
 
     return {
         renderGrid,
